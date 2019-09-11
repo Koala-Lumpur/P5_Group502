e62110cf5941dec0a07445d7aa30cdd319888f1b
Simon  Rostami <srosta17@student.aau.dk>
2019-09-11 15:25:32 +0200

    Box data to max
    Move box using arrow keys

---
+++ Unity/Box moving - send to max/Assets/OscSimpl/Send.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Send : MonoBehaviour
+
+{
+
+    public OscOut oscOut;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+      if (!oscOut) oscOut = gameObject.AddComponent<OscOut>();
+      oscOut.Open(8000,"127.0.0.1");
+      oscOut.Send("OSC connection established");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         oscOut.Send("x: ",transform.position.x);
+         oscOut.Send("y: ",transform.position.y);
+         oscOut.Send("z: ",transform.position.z);
+
+    }
+}
