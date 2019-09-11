135edf22ba3aae1847bb3e48db6e98ff95db5f18
abargum <abargu17@student.aau.dk>
2019-09-11 13:14:16 +0200

    UdpToOsc

---
+++ Unity/UdpToOsc/Assets/OscSimpl/Send.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Send : MonoBehaviour
+
+{
+
+    public OscOut oscOut;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+      if (!oscOut) oscOut = gameObject.AddComponent<OscOut>();
+      oscOut.Open(8000,"127.0.0.1");
+      oscOut.Send("fuck simon");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
