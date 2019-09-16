ea1ecd3110a6a7dc858338268b1c36e986c89123
Oddur <litillstrakur@gmail.com>
2019-09-16 13:33:41 +0200

    New movement patch, sends angles

--- Unity/Box moving - send to max/Assets/OscSimpl/Send.cs
+++ Unity/Box moving - send to max/Assets/OscSimpl/Send.cs
@@ -9,6 +9,8 @@ public class Send : MonoBehaviour
 {
 
     public OscOut oscOut;
+    public float myAngle;
+    public float myYAngle;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +19,8 @@ public class Send : MonoBehaviour
       if (!oscOut) oscOut = gameObject.AddComponent<OscOut>();
       oscOut.Open(8000,"127.0.0.1");
       oscOut.Send("OSC connection established");
+      myAngle = gameObject.GetComponent<GetAngle>().angle;
+      myYAngle = gameObject.GetComponent<GetAngle>().yAngle;
 
     }
 
@@ -26,6 +30,21 @@ public class Send : MonoBehaviour
          oscOut.Send("x: ",transform.position.x);
          oscOut.Send("y: ",transform.position.y);
          oscOut.Send("z: ",transform.position.z);
+         myAngle = getXAngle();
+         myYAngle = getYAngle();
+         oscOut.Send("angle: ", myAngle);
+         oscOut.Send("y angle: ", myYAngle);
 
+
+    }
+
+    float getXAngle() {
+      float angleValue = gameObject.GetComponent<GetAngle>().angle;
+      return angleValue;
+    }
+
+     float getYAngle() {
+      float yAngleValue = gameObject.GetComponent<GetAngle>().yAngle;
+      return yAngleValue;
     }
 }
