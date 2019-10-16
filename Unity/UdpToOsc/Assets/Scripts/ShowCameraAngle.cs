1e25c2fc78b48ca0769d0a4e6f351124398aacf1
Peter Babo <pbabo17@student.aau.dk>
2019-10-16 18:31:54 +0200

    Camera angle tracking

---
+++ Unity/UdpToOsc/Assets/Scripts/ShowCameraAngle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCameraAngle : MonoBehaviour
+{
+string camRot;
+
+  void Start()
+  {
+      camRot = Camera.main.transform.rotation.eulerAngles.y.ToString();
+      gameObject.GetComponent<TextMesh>().text = camRot.ToString();
+      Debug.Log(camRot);
+  }
+}
