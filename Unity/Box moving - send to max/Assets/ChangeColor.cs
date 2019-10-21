4d3566cd2761dadc3b66db29fa6a53c99c3666d0
Oddur <litillstrakur@gmail.com>
2019-10-21 12:43:25 +0200

    2d scene

---
+++ Unity/Box moving - send to max/Assets/ChangeColor.cs
@@ -0,0 +1,31 @@
+﻿// Drag this script on the object we want to change the color of
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+
+    private Material material1;
+    // public Material material2;
+
+    bool FirstMaterial = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        material1 = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.Find("Sphere").GetComponent<ClickToActivate>().isActive) {
+             
+            material1.color = Color.red;
+ 
+         } else {
+             material1.color = Color.blue;
+         }
+    }
+}
---
+++ Unity/Box moving - send to max/Assets/PlayerMove.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+	private Rigidbody rb;
+    public float moveSensitivity = 15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if(GameObject.Find("Sphere").GetComponent<ClickToActivate>().isActive) {
+        float h = Input.GetAxis("Horizontal") * moveSensitivity;
+        float v = Input.GetAxis("Vertical") * moveSensitivity;
+
+        Vector3 vel = rb.velocity;
+        vel.x = h;
+        vel.z = v;
+        rb.velocity = vel;
+        }
+
+    }
+}
